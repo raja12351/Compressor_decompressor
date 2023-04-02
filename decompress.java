89de2c43113d3a2df32e3663a032896a5f6beff4
raja12351 <129305916+raja12351@users.noreply.github.com>
2023-04-02 20:44:21 +0530

    Update decompress.java

--- decompress.java
+++ decompress.java
@@ -8,12 +8,17 @@ import java.util.zip.GZIPInputStream;
 
 public class decompress {
     public static void method(File file) throws IOException{
+//         Storing the directory of the selected file
         String fileDirectory = file.getParent();
 
+//         file input stream to read data from the file in the form of bytes
         FileInputStream fis = new FileInputStream(file);
+//         creates a new input stream with default buffer size
         GZIPInputStream gzip = new GZIPInputStream(fis);
+//         output stream to write or store the data to the file
         FileOutputStream fos = new FileOutputStream(fileDirectory + "/decompressedFile.jpg");
 
+//         using buffer array to write the decompressed file in bytes format in output stream
         byte[] buffer = new byte[1024];
         int len;
 
@@ -21,12 +26,14 @@ public class decompress {
             fos.write(buffer,0,len);
         }
 
+//         Closing all streams and releasing any system resources associated with the streams
         gzip.close();
         fis.close();
         fos.close();
     }
 
     public static void main(String[] args) throws IOException {
+//         creating an object of File class to pass it to the method function for further operation
         File path = new File("");
         method(path);
     }

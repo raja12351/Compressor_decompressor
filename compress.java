b5dcf125dd299f9545dc5f14350f0439a2abf9d9
raja12351 <129305916+raja12351@users.noreply.github.com>
2023-04-02 20:36:35 +0530

    Update compress.java

--- compress.java
+++ compress.java
@@ -8,23 +8,30 @@ import java.util.zip.GZIPOutputStream;
 
 public class compress {
     public static void method(File file) throws IOException{
+//         Storing the directory of the selected file
         String fileDirectory = file.getParent();
 
+//         file input stream to read data from the file in the form of bytes
         FileInputStream fis = new FileInputStream(file);
+//         output stream to write or store the data to the file
         FileOutputStream fos = new FileOutputStream(fileDirectory + "/compressedFile.gz");
+//         useful for writing the compressed data in ".gz" format
         GZIPOutputStream gzip = new GZIPOutputStream(fos);
 
+//         using buffer array to write the compressed file in bytes format in gzip stream
         byte[] buffer = new byte[1024];
         int len;
 
         while((len=fis.read(buffer))!=-1){
             gzip.write(buffer,0,len);
         }
+//         closing all output streams
         gzip.close();
         fis.close();
         fos.close();
     }
     public static void main(String[] args) throws IOException{
+//         creating an object of File class to pass it to the method function for further operation
         File path = new File("");
         method(path);
     }

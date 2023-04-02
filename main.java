68116468676e251ee1ae73b7b76be107923a19fb
raja12351 <129305916+raja12351@users.noreply.github.com>
2023-04-02 20:49:43 +0530

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ package CompDecomp.GUI;
 
 public class main {
     public static void main(String[] args) {
+//         creating an object of appframe class to execute the progran
         AppFrame frame = new AppFrame();
     }
 }
